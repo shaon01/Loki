a11dad7eed7459ff425e552e84ca63c802a3be81
shaon01 <shaoun46@gmail.com>
2020-08-22 20:30:38 +0200

    initial ros implementatin of driving Loki

---
+++ Arduino_driver/Loki_ros_main/Loki_ros_main.ino
@@ -0,0 +1,56 @@
+/*
+ * rosserial PubSub Example
+ * Drives the robot in different direction
+ */
+
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <Loki.h>
+
+ros::NodeHandle  nh;
+Loki evilLoki = Loki();
+
+// call back function for navigation masage
+void lokiDriveDirection( const std_msgs::String& directionMsg);
+// creating the subscriber topi with name LokiDriveDirection
+ros::Subscriber<std_msgs::String> sub("Loki_drive_direction", lokiDriveDirection );
+
+// Publisher massage 
+std_msgs::String str_msg;
+ros::Publisher Sensor_value("Distance_sensor", &str_msg);
+char hello[13] = "hello world!";
+
+void setup()
+{
+  pinMode(13, OUTPUT);
+  nh.initNode();
+  nh.advertise(Sensor_value);
+  nh.subscribe(sub);
+}
+
+void loop()
+{
+  str_msg.data = hello;
+  Sensor_value.publish( &str_msg );
+  nh.spinOnce();
+  delay(500);
+}
+
+void lokiDriveDirection( const std_msgs::String& directionMsg){
+  
+  if (strcmp(directionMsg.data, "Forward") == 0)
+  {
+    evilLoki.go_advance();
+    nh.loginfo("Moving Robot Forward");
+  }
+  else if (strcmp(directionMsg.data, "Stop") == 0) 
+  {
+    evilLoki.stop_Stop();
+    nh.loginfo("Stopping the robot");
+  }
+
+  
+  nh.loginfo("Got new data");
+  nh.loginfo(directionMsg.data);
+  
+}
